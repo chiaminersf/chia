2ba35799dfb6693fed2f0a143ce002ef8fec14c1
chiaminersf <83688360+chiaminersf@users.noreply.github.com>
2021-05-06 19:51:39 -0700

    Create upgrade_chia.sh

---
+++ upgrade_chia.sh
@@ -0,0 +1,31 @@
+
+# kill existing process
+ps aux | grep chia |  awk '{print $2}'  |  xargs sudo kill -9
+
+cd chia-blockchain
+chia stop -d all
+deactivate
+git fetch
+git checkout latest
+git reset --hard FETCH_HEAD
+
+# If you get RELEASE.dev0 then delete the package-lock.json in chia-blockchain-gui and install.sh again
+
+git status
+
+# git status should say "nothing to commit, working tree clean", 
+# if you have uncommitted changes, RELEASE.dev0 will be reported.
+
+sh install.sh
+
+. ./activate
+
+chia init
+
+# The GUI requires you have Ubuntu Desktop or a similar windowing system installed.
+# You can not install and run the GUI as root
+cd chia-blockchain-gui
+git fetch
+cd ..
+chmod +x ./install-gui.sh
+./install-gui.sh
