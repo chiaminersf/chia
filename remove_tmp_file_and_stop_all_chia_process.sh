0dde68887cf69ca562cc963364a23216ed1207fa
chiaminersf <83688360+chiaminersf@users.noreply.github.com>
2021-05-07 22:22:19 -0700

    Update remove_tmp_file_and_stop_all_chia_process.sh

--- remove_tmp_file_and_stop_all_chia_process.sh
+++ remove_tmp_file_and_stop_all_chia_process.sh
@@ -1,4 +1,4 @@
 find /media/eric/swap1/ -name '*.tmp' -type f -delete
 find /media/eric/swap2/ -name '*.tmp' -type f -delete
 find /media/eric/swap/ -name '*.tmp' -type f -delete
-ps aux | grep chia | grep -v grep |  awk '{print $2}'  |  xargs sudo kill -9
+ps aux | grep -e chia -e main.py | grep -v grep |  awk '{print $2}'  |  xargs sudo kill -9
