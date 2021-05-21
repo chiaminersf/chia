dfd5eae98a84f248f18fb5a479d9a84a5ea745c3
chiaminersf <83688360+chiaminersf@users.noreply.github.com>
2021-05-21 00:10:23 -0700

    Update install_plotman_after_chia.sh

--- install_plotman_after_chia.sh
+++ install_plotman_after_chia.sh
@@ -4,3 +4,4 @@ cd ~/chia-blockchain
 pip install --force-reinstall git+https://github.com/chiaminersf/plotman.git
 plotman config generate
 cp ~/chiaprivatesettings/eric1/plotman.yaml ~/.config/plotman/plotman.yaml
+echo "copied ~/chiaprivatesettings/eric1/plotman.yaml to ~/.config/plotman/plotman.yaml"
