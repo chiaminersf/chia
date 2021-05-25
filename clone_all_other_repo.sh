1322b0eba44af022de893504992e0c6d09d4c37b
chiaminersf <83688360+chiaminersf@users.noreply.github.com>
2021-05-25 00:05:39 -0700

    Update clone_all_other_repo.sh

--- clone_all_other_repo.sh
+++ clone_all_other_repo.sh
@@ -2,3 +2,4 @@ cd
 git clone https://github.com/chiaminersf/plotman.git
 git clone https://github.com/chiaminersf/chiadog.git
 git clone https://github.com/chiaminersf/chiaprivatesettings.git
+git clone https://github.com/chiaminersf/metricreporter.git
