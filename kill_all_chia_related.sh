6d79ece565616ad740bb1d6ec02fd395fee558da
chiaminersf <83688360+chiaminersf@users.noreply.github.com>
2021-07-11 12:41:45 -0700

    Update kill_all_chia_related.sh

--- kill_all_chia_related.sh
+++ kill_all_chia_related.sh
@@ -1,1 +1,1 @@
-ps aux | grep -e 'chia' -e main.py | grep -v grep |  awk '{print $2}'  |  xargs sudo kill -9
+ps aux | grep -e 'chia' -e chiaminersf_reporter.py | grep -v grep |  awk '{print $2}'  |  xargs sudo kill -9
