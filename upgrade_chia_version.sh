945baa733f5e8182b6b3b88e90e2552d47ff0f76
chiaminersf <83688360+chiaminersf@users.noreply.github.com>
2021-07-11 21:34:19 -0700

    Update upgrade_chia_version.sh

--- upgrade_chia_version.sh
+++ upgrade_chia_version.sh
@@ -1,7 +1,3 @@
-echo "Delete All chia related process first"
-sleep 2
-ps aux | grep -e 'chia' -e chiaminersf_reporter.py | grep -v grep |  awk '{print $2}'  |  xargs sudo kill -9
-
 
 cd
 cd chia-blockchain
